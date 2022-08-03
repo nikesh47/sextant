9ba2bf9ec4cad7520f7703a0aeda447e80cd41b9
nikesh47 <nik3sh47@gmail.com>
2022-08-03 12:40:46 +0100

    Add Banner and Exhibit and change code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import React,{Component} from 'react';
 import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
+      <Banner bannerText="Sextant" />
+      <Exhibit name="I'm a First Exhibit"></Exhibit>
+      <Exhibit name="I'm a Second Exhibit"></Exhibit>
+      <Exhibit name="I'm a Third Exhitbit"></Exhibit>
       
      
     </div>
